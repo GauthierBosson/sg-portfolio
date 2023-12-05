b5a95967eaa469e90a1f3a4fc9900e033a8b7d36
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-12-05 14:50:51 +0100

    Added prettier and eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "all",
+  jsxBracketSameLine: true,
+  bracketSpacing: false,
+  parser: "typescript",
+  printWidth: 80,
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+};
+
+export default config;
