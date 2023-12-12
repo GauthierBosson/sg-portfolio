e89262c8fffe91f5c86b0d082f1086a5c6cf3ef8
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-12-12 15:49:01 +0100

    Added layout elements

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,7 +16,17 @@ export default function Index() {
       <main>
         <section
           className={'h-[calc(100vh-74px)] grid place-items-center relative'}>
-          <div>
+          <div className={'relative'}>
+            <div aria-hidden={true} className={'absolute inset-0 z-[-1]'}>
+              <span
+                className={
+                  'bg-green-200 w-[300px] h-[300px] rounded-full blur-3xl absolute top-1/2 transform -translate-y-1/2'
+                }></span>
+              <span
+                className={
+                  'bg-blue-200 w-[300px] h-[300px] rounded-full blur-3xl top-1/2 absolute left-1/2 transform -translate-y-1/2'
+                }></span>
+            </div>
             <p className={'text-[24px] text-center'}>Sandra Gonzalez</p>
             <h1
               className={
@@ -24,7 +34,7 @@ export default function Index() {
               }>
               Senior Designer
             </h1>
-            <ul className={'flex gap-10 text-[16px]'}>
+            <ul className={'flex justify-between text-[16px]'}>
               <li>UI/UX desgin</li>
               <li>Design systems</li>
               <li>Mobile/Web apps</li>
@@ -36,6 +46,28 @@ export default function Index() {
             Scroll
             <ChevronDown />
           </div>
+        </section>
+        <section className={'container mx-auto py-[85px]'}>
+          <h2 className={'text-center text-[42px]'}>Porfolio</h2>
+          <article className={'pt-[85px] pb-[150px]'}>
+            <img
+              src="/flowbank_trading_app/flowbank_trading_app_4x.webp"
+              alt="App de trading flowbank"
+              className={'rounded'}
+            />
+            <h3>App de trading Flowbank</h3>
+            <h4>Design system / UI/UX design / User testing</h4>
+          </article>
+
+          <div className={'grid grid-cols-2 gap-4'}>
+            <div className={'even:pt-[250px]'}>sad</div>
+            <div className={'even:pt-[250px]'}>cvgf</div>
+            <div className={'even:pt-[250px]'}>fdgt</div>
+            <div className={'even:pt-[250px]'}>fdgdf</div>
+          </div>
+        </section>
+        <section className={'container mx-auto'}>
+          <h2 className={'text-[42px]'}>A propos</h2>
         </section>
       </main>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,34 @@ export default {
       fontFamily: {
         sans: ['Gabarito', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        'spin-around-left': {
+          '0%': {
+            transform: 'rotate(0deg) translate(10px) translateX(0)',
+          },
+          '50%': {
+            transform: 'rotate(180deg) translate(60px) translateX(50%)',
+          },
+          '100%': {
+            transform: 'rotate(360deg) translate(60px) translateX(0)',
+          },
+        },
+        'spin-around-right': {
+          '0%': {
+            transform: 'rotate(0deg) translate(50px) translateX(0)',
+          },
+          '50%': {
+            transform: 'rotate(180deg) translate(50px) translateX(50%)',
+          },
+          '100%': {
+            transform: 'rotate(360deg) translate(50px) translateX(0)',
+          },
+        },
+      },
+      animation: {
+        'spin-around-left': 'spin-around-left 6s linear infinite offset-1s',
+        'spin-around-right': 'spin-around-right 6s linear infinite',
+      },
     },
   },
   plugins: [],
